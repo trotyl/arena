f6c131510d33e7e19a637f8ad0eb5c92584f87bf
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-01 18:34:17 +0800

    Add test of game.

--- src/main/java/me/trotyl/arena/Game.java
+++ src/main/java/me/trotyl/arena/Game.java
@@ -2,4 +2,35 @@ package me.trotyl.arena;
 
 
 public class Game {
+    private Player player1;
+    private Player player2;
+    private boolean inTurnOfPlayer1;
+
+    public Game() {
+        init();
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+        init();
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+        init();
+    }
+
+    public Player run() {
+        while (player1.isAlive() && player2.isAlive()) {
+            Player attacker = inTurnOfPlayer1? player1: player2;
+            Player defender = inTurnOfPlayer1? player2: player1;
+
+            attacker.attack(defender);
+        }
+        return player1.isAlive()? player2: player1;
+    }
+
+    private void init() {
+        inTurnOfPlayer1 = true;
+    }
 }
---
+++ src/main/java/me/trotyl/arena/Player.java
@@ -0,0 +1,18 @@
+package me.trotyl.arena;
+
+
+public class Player {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isAlive() {
+        return false;
+    }
+
+    public void attack(Player another) {
+
+    }
+}
