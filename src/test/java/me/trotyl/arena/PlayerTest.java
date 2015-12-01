ac6fae0d8333b3d9ca47b0d53db840b07282ac2d
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-01 18:46:02 +0800

    Add test of player.

--- src/test/java/me/trotyl/arena/PlayerTest.java
+++ src/test/java/me/trotyl/arena/PlayerTest.java
@@ -21,10 +21,22 @@ public class PlayerTest {
 
     @Test
     public void should_be_alive_when_health_is_greater_than_zero() throws Exception {
-        Player player = new Player("张三", 10, 5);
+        Player player = new Player("张三", 1, 5);
 
         boolean isAlive = player.isAlive();
 
         assertThat(isAlive, is(true));
+    }
+
+    @Test
+    public void should_not_be_alive_when_health_is_not_greater_than_zero() throws Exception {
+        Player player1 = new Player("张三", 0, 5);
+        Player player2 = new Player("张三", -1, 5);
+
+        boolean isPlayer1Alive = player1.isAlive();
+        boolean isPlayer2Alive = player2.isAlive();
+
+        assertThat(isPlayer1Alive, is(false));
+        assertThat(isPlayer2Alive, is(false));
     }
 }
