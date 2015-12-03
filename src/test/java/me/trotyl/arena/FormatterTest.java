f9ce554d185681fb2536be2718cf2cd9bdd7ab6b
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-03 21:55:46 +0800

    Add test for Formatter.

--- src/test/java/me/trotyl/arena/FormatterTest.java
+++ src/test/java/me/trotyl/arena/FormatterTest.java
@@ -2,8 +2,10 @@ package me.trotyl.arena;
 
 import me.trotyl.arena.procedure.AttackProcedure;
 import me.trotyl.arena.procedure.EffectProcedure;
+import me.trotyl.arena.record.ArmorRecord;
 import me.trotyl.arena.record.DamageRecord;
 import me.trotyl.arena.record.PlayerRecord;
+import me.trotyl.arena.record.WeaponRecord;
 import me.trotyl.arena.role.*;
 import org.javatuples.Pair;
 import org.junit.After;
@@ -46,12 +48,25 @@ public class FormatterTest {
 
     @Test
     public void format_attack_should_have_proper_result_without_weapon_nor_attribute() {
-        AttackProcedure procedure = new AttackProcedure(new PlayerRecord("张三", 10, Role.soldier),
+        AttackProcedure procedure = new AttackProcedure(
+                new PlayerRecord("张三", 10, Role.soldier),
                 new PlayerRecord("李四", 20, Role.normal),
                 new DamageRecord(5));
 
         String result = formatter.formatAttack(procedure);
 
         assertThat(result, is("战士张三攻击了普通人李四, 李四受到了5点伤害, 李四剩余生命: 20"));
+    }
+
+    @Test
+    public void format_attack_should_have_proper_result_with_weapon() {
+        AttackProcedure procedure = new AttackProcedure(
+                new PlayerRecord("张三", 10, Role.soldier, new WeaponRecord("优质木棒"), ArmorRecord.none),
+                new PlayerRecord("李四", 20, Role.normal),
+                new DamageRecord(5));
+
+        String result = formatter.formatAttack(procedure);
+
+        assertThat(result, is("战士张三用优质木棒攻击了普通人李四, 李四受到了5点伤害, 李四剩余生命: 20"));
     }
 }
