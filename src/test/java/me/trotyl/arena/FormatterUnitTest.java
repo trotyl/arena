9af6e323139e56dff347fdeafe9c5587d6979382
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-03 11:00:25 +0800

    Rename FormatterTest to FormatterUnitTest.

--- src/test/java/me/trotyl/arena/FormatterUnitTest.java
+++ src/test/java/me/trotyl/arena/FormatterUnitTest.java
@@ -10,7 +10,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 
-public class FormatterTest {
+public class FormatterUnitTest {
 
     private Formatter formatter;
 
