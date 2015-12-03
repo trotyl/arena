6b0e5787bf184467efd989512abcb319bc62dd35
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-03 11:19:26 +0800

    Seperate private field for FormatterUnitTest.

--- src/test/java/me/trotyl/arena/FormatterUnitTest.java
+++ src/test/java/me/trotyl/arena/FormatterUnitTest.java
@@ -16,6 +16,12 @@ import static org.junit.Assert.assertThat;
 public class FormatterUnitTest {
 
     private Formatter formatter;
+    private Player player;
+    private Player anotherPlayer;
+    private Soldier soldierWithoutEquipment;
+    private Soldier soldierWithWeapon;
+    private Soldier soldierWithArmor;
+    private Soldier soldierWithWeaponAndArmor;
 
     private String formattedAttack(Attacker attacker, Attackable attackable) {
         AttackProcedure attack = attacker.attack(attackable);
@@ -25,6 +31,17 @@ public class FormatterUnitTest {
     @Before
     public void setUp() throws Exception {
         formatter = new Formatter();
+
+        player = new Player("张三", 10, 5);
+        anotherPlayer = new Player("李四", 20, 8);
+        soldierWithoutEquipment = new Soldier("李四", 20, 8);
+        soldierWithWeapon = new Soldier("李四", 20, 8);
+        soldierWithWeapon.equip(new Weapon("优质木棒", 5));
+        soldierWithArmor = new Soldier("李四", 20, 8);
+        soldierWithArmor.equip(new Armor(3));
+        soldierWithWeaponAndArmor = new Soldier("李四", 20, 8);
+        soldierWithWeaponAndArmor.equip(new Weapon("优质木棒", 5));
+        soldierWithWeaponAndArmor.equip(new Armor(3));
     }
 
     @After
@@ -34,75 +51,49 @@ public class FormatterUnitTest {
 
     @Test
     public void should_work_of_attack_between_normal_players() {
-        Player player1 = new Player("张三", 10, 5);
-        Player player2 = new Player("李四", 20, 8);
-
-        String result = formattedAttack(player1, player2);
+        String result = formattedAttack(player, anotherPlayer);
 
         assertThat(result, is("普通人张三攻击了普通人李四, 李四受到了5点伤害, 李四剩余生命: 15"));
     }
 
     @Test
     public void should_work_of_attack_between_normal_player_and_soldier_without_equipment() {
-        Player player1 = new Player("张三", 10, 5);
-        Soldier player2 = new Soldier("李四", 20, 8);
-
-        String result = formattedAttack(player1, player2);
+        String result = formattedAttack(player, soldierWithoutEquipment);
 
         assertThat(result, is("普通人张三攻击了战士李四, 李四受到了5点伤害, 李四剩余生命: 15"));
     }
 
     @Test
     public void should_work_of_attack_between_normal_player_and_soldier_with_weapon_but_no_armor() {
-        Player player1 = new Player("张三", 10, 5);
-        Soldier player2 = new Soldier("李四", 20, 8);
-        player2.equip(new Weapon("优质木棒", 5));
-
-        String result = formattedAttack(player1, player2);
+        String result = formattedAttack(player, soldierWithWeapon);
 
         assertThat(result, is("普通人张三攻击了战士李四, 李四受到了5点伤害, 李四剩余生命: 15"));
     }
 
     @Test
     public void should_work_of_attack_between_normal_player_and_soldier_with_armor_but_no_weapon() {
-        Player player1 = new Player("张三", 10, 5);
-        Soldier player2 = new Soldier("李四", 20, 8);
-        player2.equip(new Armor(3));
-
-        String result = formattedAttack(player1, player2);
+        String result = formattedAttack(player, soldierWithArmor);
 
         assertThat(result, is("普通人张三攻击了战士李四, 李四受到了2点伤害, 李四剩余生命: 18"));
     }
 
     @Test
     public void should_work_of_attack_between_normal_player_and_soldier_with_weapon_and_armor() {
-        Player player1 = new Player("张三", 10, 5);
-        Soldier player2 = new Soldier("李四", 20, 8);
-        player2.equip(new Weapon("优质木棒", 5));
-        player2.equip(new Armor(3));
-
-        String result = formattedAttack(player1, player2);
+        String result = formattedAttack(player, soldierWithWeaponAndArmor);
 
         assertThat(result, is("普通人张三攻击了战士李四, 李四受到了2点伤害, 李四剩余生命: 18"));
     }
 
     @Test
     public void should_work_of_attack_between_soldier_without_equipment_and_normal_player() {
-        Player player1 = new Player("张三", 10, 5);
-        Soldier player2 = new Soldier("李四", 20, 8);
-
-        String result = formattedAttack(player2, player1);
+        String result = formattedAttack(soldierWithoutEquipment, player);
 
         assertThat(result, is("战士李四攻击了普通人张三, 张三受到了8点伤害, 张三剩余生命: 2"));
     }
 
     @Test
     public void should_work_of_attack_between_soldier_with_weapon_but_no_armor_and_normal_player() {
-        Player player1 = new Player("张三", 10, 5);
-        Soldier player2 = new Soldier("李四", 20, 8);
-        player2.equip(new Weapon("优质木棒", 5));
-
-        String result = formattedAttack(player2, player1);
+        String result = formattedAttack(soldierWithWeapon, player);
 
         assertThat(result, is("战士李四用优质木棒攻击了普通人张三, 张三受到了13点伤害, 张三剩余生命: -3"));
     }
