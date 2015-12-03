1a7cb9bf3398ce73c19acb75322da2ce9942d634
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-03 19:46:54 +0800

    Add test for Effect.

--- src/test/java/me/trotyl/arena/effect/EffectTest.java
+++ src/test/java/me/trotyl/arena/effect/EffectTest.java
@@ -1,8 +1,12 @@
 package me.trotyl.arena.effect;
 
+import me.trotyl.arena.attribute.Attribute;
+import me.trotyl.arena.attribute.Genre;
 import me.trotyl.arena.record.DamageRecord;
 import me.trotyl.arena.record.EffectRecord;
 import me.trotyl.arena.record.PlayerRecord;
+import me.trotyl.arena.role.Attackable;
+import me.trotyl.arena.role.Attacker;
 import me.trotyl.arena.role.Player;
 import org.junit.After;
 import org.junit.Before;
@@ -19,6 +23,7 @@ public class EffectTest {
     private Effect effect;
     private Player player1;
     private Player player2;
+    private Attribute attribute;
 
     @Before
     public void setUp() throws Exception {
@@ -26,6 +31,13 @@ public class EffectTest {
 
         player1 = spy(new Player("张三", 10, 5));
         player2 = spy(new Player("李四", 20, 8));
+
+        attribute = spy(new Attribute(-1, 0.0f) {
+            @Override
+            public DamageRecord apply(Attacker attacker, Attackable attackable) {
+                return applyNoEffect(attacker, attackable);
+            }
+        });
     }
 
     @After
@@ -34,7 +46,7 @@ public class EffectTest {
     }
 
     @Test
-    public void should_be_taken_proper_effect() {
+    public void take_should_have_proper_result() {
         DamageRecord damage = effect.take(player1);
         PlayerRecord player1Record = player1.record();
         PlayerRecord player2Record = player2.record();
@@ -46,16 +58,24 @@ public class EffectTest {
     }
 
     @Test
-    public void should_be_taken_with_proper_invocation() {
+    public void take_should_have_proper_invocation() {
         effect.take(player1);
 
         verifyZeroInteractions(player1);
     }
 
     @Test
-    public void should_be_record_proper_result() {
+    public void record_should_have_proper_result() {
         EffectRecord record = effect.record();
 
         assertThat(record, is(EffectRecord.none));
+    }
+
+    @Test
+    public void sway_should_have_proper_result() {
+        DamageRecord damage = effect.sway(player1, player2, attribute);
+
+        assertThat(damage.genre, is(Genre.none));
+        assertThat(damage.extent, is(5));
     }
 }
