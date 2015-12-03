d3be897bef2cdeb49a7648d00a77eea7f968ac6e
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-03 21:16:26 +0800

    Update test for Soldier.

--- src/test/java/me/trotyl/arena/role/SoldierTest.java
+++ src/test/java/me/trotyl/arena/role/SoldierTest.java
@@ -3,21 +3,28 @@ package me.trotyl.arena.role;
 import me.trotyl.arena.Armor;
 import me.trotyl.arena.Weapon;
 import me.trotyl.arena.attribute.Dizzy;
+import me.trotyl.arena.attribute.Genre;
 import me.trotyl.arena.attribute.Toxic;
 import me.trotyl.arena.attribute.Striking;
+import me.trotyl.arena.effect.Effect;
 import me.trotyl.arena.effect.Swoon;
 import me.trotyl.arena.effect.Toxin;
+import me.trotyl.arena.procedure.AttackProcedure;
+import me.trotyl.arena.procedure.EffectProcedure;
+import me.trotyl.arena.record.DamageRecord;
+import me.trotyl.arena.record.EffectRecord;
+import org.javatuples.Pair;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 
 import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 public class SoldierTest {
@@ -61,6 +68,34 @@ public class SoldierTest {
         soldier0.equip(newWeapon);
         assertThat(soldier0.weapon, is(newWeapon));
         assertThat(soldier0.armor, is(newArmor));
+    }
+
+    @Test
+    public void attack_should_have_proper_result() {
+        Soldier soldier2 = spy(soldier0);
+        Soldier soldier3 = spy(soldier1);
+
+        Pair<EffectProcedure, AttackProcedure> pair = soldier2.attack(soldier3);
+        EffectProcedure effectProcedure = pair.getValue0();
+        AttackProcedure attackProcedure = pair.getValue1();
+
+        assertThat(effectProcedure.attackable.name(), is("张三"));
+        assertThat(effectProcedure.attackable.health(), is(10));
+        assertThat(effectProcedure.damage, is(DamageRecord.none));
+        assertThat(effectProcedure.effect, is(EffectRecord.none));
+        assertThat(attackProcedure.attacker.name(), is("张三"));
+        assertThat(attackProcedure.attackable.name(), is("李四"));
+        assertThat(attackProcedure.attackable.health(), is(15));
+        assertThat(attackProcedure.damage.genre, is(Genre.none));
+        assertThat(attackProcedure.damage.extent, is(5));
+
+        InOrder inOrder = inOrder(soldier2, soldier3);
+        inOrder.verify(soldier2).record();
+        inOrder.verify(soldier2).aggressivity();
+        inOrder.verify(soldier3).defence();
+        inOrder.verify(soldier3).suffer(5, Effect.none);
+        inOrder.verify(soldier2).record();
+        inOrder.verify(soldier3).record();
     }
 
     @Test
