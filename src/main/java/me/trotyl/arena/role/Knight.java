d01cee36d7ccbd405d1f8aeb555e339fe3d7b052
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-04 01:30:53 +0800

    Add test for Knight.

---
+++ src/main/java/me/trotyl/arena/role/Knight.java
@@ -0,0 +1,20 @@
+package me.trotyl.arena.role;
+
+
+import me.trotyl.arena.weapon.Length;
+import me.trotyl.arena.weapon.Weapon;
+
+public class Knight extends Soldier {
+
+    public Knight(String name, int health, int aggressivity) {
+        super(name, health, aggressivity);
+    }
+
+    @Override
+    public void equip(Weapon weapon) {
+        if (weapon.length() != Length.longer) {
+            throw new IllegalArgumentException("Knight can only equip long weapon!");
+        }
+        super.equip(weapon);
+    }
+}
---
+++ src/test/java/me/trotyl/arena/role/KnightTest.java
@@ -0,0 +1,52 @@
+package me.trotyl.arena.role;
+
+import me.trotyl.arena.weapon.Length;
+import me.trotyl.arena.weapon.Weapon;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+
+public class KnightTest {
+
+    private Knight knight;
+
+    @Before
+    public void setUp() throws Exception {
+        knight = new Knight("张三", 10, 5);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void equip_should_have_proper_result() {
+        Weapon shortWeapon = new Weapon("诸葛连弩", 3, Length.shorter);
+        Weapon middleWeapon = new Weapon("雌雄双股剑", 4, Length.medium);
+        Weapon longWeapon = new Weapon("方天画戟", 5, Length.longer);
+
+        knight.equip(longWeapon);
+
+        assertThat(knight.weapon, is(longWeapon));
+
+        try {
+            knight.equip(shortWeapon);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), is("Knight can only equip long weapon!"));
+        }
+
+        try {
+            knight.equip(middleWeapon);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), is("Knight can only equip long weapon!"));
+        }
+    }
+}
