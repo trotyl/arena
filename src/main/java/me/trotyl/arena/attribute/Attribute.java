527f299cd47eb4a8d8e9e6515cbf3271ad1128b4
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-04 12:31:54 +0800

    Sort signature of Attribute

--- src/main/java/me/trotyl/arena/attribute/Attribute.java
+++ src/main/java/me/trotyl/arena/attribute/Attribute.java
@@ -10,34 +10,40 @@ import java.util.Random;
 
 public abstract class Attribute {
 
+    public static Attribute none = new Attribute(-1, 0.0f) {};
     protected static Random random = new Random();
 
     public static void config(Random random) {
         Attribute.random = random;
     }
 
-    public static Attribute none = new Attribute(-1, 0.0f) {};
-
     protected int limit;
     protected float rate;
 
     protected Attribute(int limit, float rate) {
+
         this.limit = limit;
         this.rate = rate;
     }
 
+    public int limit() {
+        return limit;
+    }
+
+    public float rate() {
+        return rate;
+    }
+
     public DamageRecord apply(Attacker attacker, Attackable attackable) {
+
         int damage = attacker.aggressivity() - attackable.defence();
         attackable.suffer(damage, Effect.none);
 
         return DamageRecord.create(damage);
     }
 
-    protected boolean works() {
-        return random.nextFloat() < rate;
-    }
-
     protected DamageRecord applyByEffect(Attacker attacker, Attackable attackable, Effect effect, Genre genre) {
+
         if (!works()) {
             return none.apply(attacker, attackable);
         }
@@ -48,11 +54,7 @@ public abstract class Attribute {
         return DamageRecord.create(damage, genre);
     }
 
-    public float rate() {
-        return rate;
-    }
-
-    public int limit() {
-        return limit;
+    protected boolean works() {
+        return random.nextFloat() < rate;
     }
 }
