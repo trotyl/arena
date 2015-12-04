687b75251ce48831b5a2afca632b729200bcfc17
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-04 12:54:26 +0800

    Sort signature of Program.

--- src/main/java/me/trotyl/arena/Program.java
+++ src/main/java/me/trotyl/arena/Program.java
@@ -12,26 +12,6 @@ import java.io.*;
 
 public class Program {
 
-    private final PrintStream out;
-    private final Formatter formatter;
-    private final Game game;
-
-    public Program(InputStream in, PrintStream out, Parser parser, Formatter formatter) {
-        this.out = out;
-        this.formatter = formatter;
-
-        JSONTokener tokener = new JSONTokener(in);
-        JSONObject configObject = (JSONObject) tokener.nextValue();
-
-        JSONObject player1Object = configObject.getJSONObject("player1");
-        Player player1 = parser.parsePlayer(player1Object);
-
-        JSONObject player2Object = configObject.getJSONObject("player2");
-        Player player2 = parser.parsePlayer(player2Object);
-
-        game = Game.between(player1, player2);
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -53,6 +33,26 @@ public class Program {
         }
 
         reader.close();
+    }
+
+    private final PrintStream out;
+    private final Formatter formatter;
+    private final Game game;
+
+    public Program(InputStream in, PrintStream out, Parser parser, Formatter formatter) {
+        this.out = out;
+        this.formatter = formatter;
+
+        JSONTokener tokener = new JSONTokener(in);
+        JSONObject configObject = (JSONObject) tokener.nextValue();
+
+        JSONObject player1Object = configObject.getJSONObject("player1");
+        Player player1 = parser.parsePlayer(player1Object);
+
+        JSONObject player2Object = configObject.getJSONObject("player2");
+        Player player2 = parser.parsePlayer(player2Object);
+
+        game = Game.between(player1, player2);
     }
 
     public void run() {
