2cb99fd32c02c01a964eebea82495bcf2fc3ce9c
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-06 13:58:25 +0800

    Create test for CompositeAttribute.

---
+++ src/test/java/me/trotyl/arena/attribute/CompositeAttributeTest.java
@@ -0,0 +1,56 @@
+package me.trotyl.arena.attribute;
+
+import me.trotyl.arena.record.DamageRecord;
+import me.trotyl.arena.record.PlayerRecord;
+import me.trotyl.arena.role.Player;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+
+public class CompositeAttributeTest {
+
+    private Random random;
+    private CompositeAttribute composite;
+    private Player player1;
+    private Player player2;
+
+    @Before
+    public void setUp() throws Exception {
+        random = mock(Random.class);
+        when(random.nextFloat()).thenReturn(0.0f);
+
+        Attribute.config(random);
+
+        player1 = spy(Player.create("张三", 10, 5));
+        player2 = spy(Player.create("李四", 20, 8));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Attribute.config(new Random());
+    }
+
+    @Test
+    public void apply_should_have_proper_result_when_first_work() {
+
+        composite = new CompositeAttribute(Dizzy.create(1.0f), Flaming.create(3, 2, 1.0f));
+
+        DamageRecord damage = composite.apply(player1, player2, Attribute.none);
+
+        PlayerRecord player1Record = player1.record();
+        PlayerRecord player2Record = player2.record();
+
+        assertThat(damage.genre, is(Genre.dizzy));
+        assertThat(damage.extent, is(5));
+
+        assertThat(player1Record.getHealth(), is(10));
+        assertThat(player2Record.getHealth(), is(15));
+    }
+}
